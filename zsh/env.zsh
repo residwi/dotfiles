a4a2b78448db3d8d06cb063b74d384337d75366b
Resi Dwi <37761892+residwi@users.noreply.github.com>
2022-09-12 14:13:37 +0700

    fix: installation on Linux
    - No need gitconfig.user, use private email instead

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -7,14 +7,14 @@ export LC_ALL='en_US.UTF-8'
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 
-export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 
 # Docker
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
 
 # FZF
-export FZF_BASE=/opt/homebrew/opt/fzf
+export FZF_BASE=$(which fzf)
 export FZF_DEFAULT_OPTS='--height 40%'
 export DEVELOPMENT_TOOLS_PATH=$HOME/Development/tools
 
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,5 +1,6 @@
-export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
 
-# Google Cloud SDK
-source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
-
+  # https://github.com/Homebrew/homebrew-cask/pull/120606
+  source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+fi
