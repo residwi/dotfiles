56c91e16564ae86393abcb7fe789992e1a5181d8
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-06-18 21:54:48 +0700

    Fix indentation in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,105 +1,101 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 export DOTFILES_BASE="$HOME/dotfiles"
 
 info() {
-	echo "[ \033[00;94m..\033[0m ]  \033[00;94m$1\033[0m"
+  echo "[ \033[00;94m..\033[0m ]  \033[00;94m$1\033[0m"
 }
 
 success() {
-	echo "[ \033[00;92mOK\033[0m ]  \033[00;92m$1\033[0m"
+  echo "[ \033[00;92mOK\033[0m ]  \033[00;92m$1\033[0m"
 }
 
 fail() {
-	echo "[ \033[00;91mFAIL\033[0m ]  \033[00;91m$1\033[0m"
-	exit
+  echo "[ \033[00;91mFAIL\033[0m ]  \033[00;91m$1\033[0m"
+  exit
 }
 
 link_file() {
-	if [ -e "$2" ]; then
-		if [ "$(readlink "$2")" = "$1" ]; then
-			success "skipped, already linked $2"
-			return 0
-		else
+  if [ -e "$2" ]; then
+    if [ "$(readlink "$2")" = "$1" ]; then
+      success "skipped, already linked $2"
+      return 0
+    else
       if [[ -f "$2" ]]; then
         info "Backup file $2"
         mv "$2" "$2.backup"
         success "$2 moved to $2.backup"
       fi
-		fi
-	fi
-	ln -s "$1" "$2"
-	success "$1 linked to $2"
+    fi
+  fi
+  ln -s "$1" "$2"
+  success "$1 linked to $2"
 }
 
 install_oh_my_zsh() {
-	search_locations=(
-		"/bin/zsh"
-		"/usr/bin/zsh"
-	)
+  search_locations=("/bin/zsh" "/usr/bin/zsh")
+  for zsh_location in $search_locations; do
+    if [[ ! -f "${zsh_location}" ]]; then
+      fail "ZSH is not installed!"
+    fi
+  done
 
-	for zsh_location in $search_locations; do
-		if [[ ! -f "${zsh_location}" ]]; then
-			fail "ZSH is not installed!"
-		fi
-	done
+  info "Installing oh-my-zsh"
 
-	info "Installing oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-	success "oh-my-zsh has been installed"
+  success "oh-my-zsh has been installed"
 }
 
 install_zsh_plugins() {
-	info "Installing zsh plugin: zsh-autosuggestions"
-	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-	success "zsh-autosuggestions has been installed"
+  info "Installing zsh plugin: zsh-autosuggestions"
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  success "zsh-autosuggestions has been installed"
 
-	info "Installing zsh plugin: zsh-syntax-highlighting"
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-	success "zsh-syntax-highlighting has been installed"
+  info "Installing zsh plugin: zsh-syntax-highlighting"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+  success "zsh-syntax-highlighting has been installed"
 }
 
 setup_gitconfig() {
-	info 'Setup gitconfig'
+  info 'Setup gitconfig'
 
-	if [[ "$(git config --global --get dotfiles.managed)" != "true" ]]; then
-		mv ~/.gitconfig ~/.gitconfig.backup
-		success "Backup ~/.gitconfig to ~/.gitconfig.backup"
-	else
-		info "already managed by dotfiles"
-	fi
+  if [[ "$(git config --global --get dotfiles.managed)" != "true" ]]; then
+    mv ~/.gitconfig ~/.gitconfig.backup
+    success "Backup ~/.gitconfig to ~/.gitconfig.backup"
+  else
+    info "already managed by dotfiles"
+  fi
 
-	git config --global include.path ~/.gitconfig.global
-	git config --global dotfiles.managed true
+  git config --global include.path ~/.gitconfig.global
+  git config --global dotfiles.managed true
 
-	success "gitconfig installed"
+  success "gitconfig installed"
 }
 
 setup_symlinks() {
-	info "Setup symlinks"
+  info "Setup symlinks"
 
-	find -H "$DOTFILES_BASE" -maxdepth 3 -name '*.symlink' -not -path '*.git*' |
-		while read -r src; do
-			dst="$HOME/.$(basename "${src%.*}")"
-			link_file "$src" "$dst"
-		done
+  symlink_files=$(find -H "$DOTFILES_BASE" -maxdepth 3 -name '*.symlink' -not -path '*.git*')
+  while read src; do
+    dst="$HOME/.$(basename "${src%.*}")"
+    link_file "$src" "$dst"
+  done <<< "$symlink_files"
 }
 
 setup_tools_development() {
-	info "Setting up tools for development"
+  info "Setting up tools for development"
 
   if [[ ! -d ~/Development/tools ]]; then
     mkdir -p ~/Development/tools
     success "~/Development/tools/ has been created"
   fi
 
-	link_file "$DOTFILES_BASE/tools/docker-compose.yml" ~/Development/tools/docker-compose.yml
+  link_file "$DOTFILES_BASE/tools/docker-compose.yml" ~/Development/tools/docker-compose.yml
 }
 
 setup_work_folder() {
-	info "Setting up Work folder"
+  info "Setting up Work folder"
 
   if [[ ! -d ~/Work ]]; then
     mkdir -p ~/Work
